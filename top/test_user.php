2596fd4507cfe523835178ce2ebe3d387718e56c
example <example@example.com>
2022-06-05 00:57:35 +0900

    テストファイル作成

---
+++ top/test_user.php
@@ -0,0 +1,14 @@
+<?php
+  require_once(__DIR__."/../classes/Dao.php");
+  require_once(__DIR__."/../classes/constants.php");
+
+  try{
+    //ログインユーザー情報
+    $users_sql = 'select * from users where user_id = ?' ;
+    $users = Dao::db()->show_one_row($users_sql,array('aki'));
+  }catch(PDOException $e){
+    print('Error:'.$e->getMessage());
+    die();
+  }
+  echo {result:true,user:{firstname:".$users['data']['first_name'].",lastname:".$users['data']['last_name']."}};
+ ?>
