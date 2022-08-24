6c9732e311606edad0c7985d05189c548c6f2973
example <example@example.com>
2022-08-24 11:19:17 +0900

    accountクラスのcss変更

--- top/top.php
+++ top/top.php
@@ -64,73 +64,60 @@
 
 		<div class="container-fluid">
 		<div class="row mb-2">
-		<!-- ナビゲーションバー -->
-		<nav class="navbar navbar-expand-md navbar-light bg-light fixed-top">
-		  <!-- タイトル -->
-		  <a class="navbar-brand" href="#">
-				<img src="../image/KnownAnimalLogo.png" width="180" height="auto" class="d-inline-block align-top" alt="">
-			</a>
-		  <!-- ハンバーガーメニュー -->
-		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-		    <span class="navbar-toggler-icon"></span>
-		  </button>
-		  <!-- ナビゲーションメニュー -->
-		  <div class="collapse navbar-collapse" id="navbarNav">
-		    <ul class="navbar-nav">
-		      <li class="nav-item active">
-		        <a class="nav-link" id="entry">新規登録</a>
-		      </li>
-		      <li class="nav-item">
-		        <a class="nav-link" href='<?php echo Constants::USER_EDIT_URL?>'>ユーザー情報の編集</a>
-		      </li>
-		      <li class="nav-item">
-						<form name="logout" method="get" action="">
-							<a class="nav-link" id="logout" href='#'>ログアウト</a>
-							<input type="hidden" id="hidden" name="btn_logout" value="ログアウト">
-						 </form>
-						 <script>
-						  var a_link = document.getElementById("logout");
+		<header>
+			<!-- ナビゲーションバー -->
+			<nav class="navbar navbar-expand-md navbar-light bg-light fixed-top">
+			  <!-- タイトル -->
+			  <a class="navbar-brand" href="#">
+					<img src="../image/KnownAnimalLogo.png" width="180" height="auto" class="d-inline-block align-top" alt="">
+				</a>
+			  <!-- ハンバーガーメニュー -->
+			  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+			    <span class="navbar-toggler-icon"></span>
+			  </button>
+			  <!-- ナビゲーションメニュー -->
+			  <div class="collapse navbar-collapse" id="navbarNav">
+			    <ul class="navbar-nav">
+			      <li class="nav-item active">
+			        <a class="nav-link" id="entry">新規登録</a>
+			      </li>
+			      <li class="nav-item">
+			        <a class="nav-link" href='<?php echo Constants::USER_EDIT_URL?>'>ユーザー情報の編集</a>
+			      </li>
+					</ul>
+				</div>
+							<form name="logout" class="logout" method="get" action="">
+								<button type="button" class="btn btn-primary" id="logout">ログアウト</button>
+								<input type="hidden" id="hidden" name="btn_logout" value="ログアウト">
+							 </form>
+							 <script>
+							  var a_link = document.getElementById("logout");
 
-						  a_link.addEventListener('click', function() {
-						    //submit()でフォームの内容を送信
-						    document.logout.submit();
-						  })
-							console.dir(document.getElementById("hidden"));
-						</script>
-
-		      </li>
-		    </ul>
-		  </div>
-		</nav>
+							  a_link.addEventListener('click', function() {
+							    //submit()でフォームの内容を送信
+							    document.logout.submit();
+							  })
+								console.dir(document.getElementById("hidden"));
+							</script>
+			</nav>
+		</header>
 		</div>
 		<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
  		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
  		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 
 		<div class="row mt-5 mb-2">
+			<div class="offset-sm-10 col-sm-2">
 			<br>
 			<p class="account">
 			</p>
+			</div>
 		</div>
 			<div class="row">
 				<div class="card-group" id="loop">
 				</div>
 			</div>
 		</div>
-
-		<div id="easyModal" class="modal">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1>Great job</h1>
-        <span class="modalClose">×</span>
-      </div>
-      <div class="modal-body">
-        <p>You've just displayed this awesome Modal Window!</p>
-        <p>Let's enjoy learning JavaScript ☺️</p>
-      </div>
-    </div>
-  </div>
-
 		<script type="module">
 			import { WebApi,LoadingCircle,Modal } from "../js/common.js";
 			const lc = new LoadingCircle({});
